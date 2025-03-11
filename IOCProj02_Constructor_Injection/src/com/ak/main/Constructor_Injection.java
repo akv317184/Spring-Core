145aeac780cd855959029b77d929fdef09ea404c
Ankit Kumar Verma <122468381+akv317184@users.noreply.github.com>
2025-03-11 10:01:15 +0530

    Constructor_Injection

---
+++ IOCProj02_Constructor_Injection/src/com/ak/main/Constructor_Injection.java
@@ -0,0 +1,24 @@
+package com.ak.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ak.config.AppConfig;
+import com.ak.sbeans.WishMessageGenerator;
+
+public class Constructor_Injection {
+
+	public static void main(String[] args) {
+		// Create the IOC Container
+		AnnotationConfigApplicationContext ctx =
+				new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		// Get Target spring bean class obj form the container
+		WishMessageGenerator generator = ctx.getBean("wmg",WishMessageGenerator.class);
+		// invoke b.method
+		String resutlMsg = generator.showWishMessage("Raja");
+		System.out.println(resutlMsg);
+		
+		// close the container
+		ctx.close();
+	}
+}
