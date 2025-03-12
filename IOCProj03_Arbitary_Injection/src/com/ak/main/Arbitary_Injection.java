a8bc98589776c6f7186cff54aba55ec30faa5a7f
Ankit Kumar Verma <122468381+akv317184@users.noreply.github.com>
2025-03-12 09:11:21 +0530

    Arbitary_Injection

--- IOCProj03_Arbitary_Injection/src/com/ak/main/Arbitary_Injection.java
+++ IOCProj03_Arbitary_Injection/src/com/ak/main/Arbitary_Injection.java
@@ -9,15 +9,14 @@ public class Arbitary_Injection {
 
 	public static void main(String[] args) {
 		// Create the IOC Container
-		AnnotationConfigApplicationContext ctx =
-				new AnnotationConfigApplicationContext(AppConfig.class);
-		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
 		// Get Target spring bean class obj form the container
-		WishMessageGenerator generator = ctx.getBean("wmg",WishMessageGenerator.class);
+		WishMessageGenerator generator = ctx.getBean("wmg", WishMessageGenerator.class);
 		// invoke b.method
 		String resutlMsg = generator.showWishMessage("Raja");
 		System.out.println(resutlMsg);
-		
+
 		// close the container
 		ctx.close();
 	}
--- IOCProj03_Arbitary_Injection/src/com/ak/sbeans/WishMessageGenerator.java
+++ IOCProj03_Arbitary_Injection/src/com/ak/sbeans/WishMessageGenerator.java
@@ -9,23 +9,26 @@ import org.springframework.stereotype.Component;
 @Component("wmg")
 public class WishMessageGenerator {
 
-	private LocalDateTime ldt;  // HAS-A properties
-	
+	private LocalDateTime ldt; // HAS-A properties
+
 	// Arbitary Injection
-	@Autowired	
+	@Autowired
 	private void assigData(LocalDateTime ldt) {
 		this.ldt = ldt;
 	}
-	
+
 	// b.method having b.logic
-	public String showWishMessage(String  user) {
+	public String showWishMessage(String user) {
 		// get current hour of the day
 		int hour = ldt.getHour();
 		// generate the wish message
-		if(hour<12) return "Good Morning : " +user;
-		else if(hour<16) return "Good Afternoon : "+user;
-		else if(hour<20 ) return "Good Evening : "+user;
-		else return "Good Night : "+user;
+		if (hour < 12)
+			return "Good Morning : " + user;
+		else if (hour < 16)
+			return "Good Afternoon : " + user;
+		else if (hour < 20)
+			return "Good Evening : " + user;
+		else
+			return "Good Night : " + user;
 	}
-	}
-	
+}
